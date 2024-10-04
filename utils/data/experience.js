2e235db776ea2b8c9d4b312bb97d46981f7ce5bc
Apple <apple@Apples-MacBook-Pro.local>
2024-10-04 19:30:21 +0530

    first commit

---
+++ utils/data/experience.js
@@ -0,0 +1,14 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Web specialist',
+    company: "UVJ Technologies",
+    duration: "(Jul 2023 - Present)"
+  },
+  {
+    id: 2,
+    title: "Front-end Developer",
+    company: "Greeniit",
+    duration: "(Jun 2021 - Jan 2022)"
+  }
+]
