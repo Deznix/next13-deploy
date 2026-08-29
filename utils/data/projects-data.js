bde83896b80d8dc144e6aac4a2ffc9696dc3c16a
Apple <apple@Apples-MacBook-Pro.local>
2026-08-29 13:25:29 +0530

    Update portfolio projects

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -6,16 +6,6 @@ import travel from '/public/image/travel.jpg';
 export const projectsData = [
     {
         id: 1,
-        name: 'Revvity',
-        description: "Led content migration ensuring data integrity and smooth transition., Managed PIM, DAM systems for effective content organization. Administered and optimized CMS platforms (Acquia, WordPress, SDL Tridion). Enhanced website functionality and user experience.",
-        tools: ['Acquia', 'PIM', 'Aprimo DAM'],
-        role: 'Web Specialist',
-        code: '',
-        demo: 'https://revvity.com',
-        image: 'image/work1.jpg',
-    },
-    {
-        id: 2,
         name: 'Greeniit',
         description: 'I have designed and developed a web app for Greeniit, an IT company in Oman. I created the UI using React, Typescript, Bootstrap, Google Maps, SCSS, and VS Code. ',
         tools: ['React', 'Bootstrap', "SCSS", "VS Code"],
@@ -25,7 +15,7 @@ export const projectsData = [
         image: 'image/work2.jpg',
     },
     {
-        id: 3,
+        id: 2,
         name: 'Makan Real Estate',
         description: 'My team built a real estate website using HTML, CSS, Bootstrap, Jquery, php, Codigniter and MySQL.',
         tools: ['HTML','CSS', 'Bootstrap', 'javascript', 'jQuery', 'php', 'CodeIgniter', 'MySQL'],
@@ -35,7 +25,7 @@ export const projectsData = [
         image: 'image/work3.jpg',
     },
     {
-        id: 4,
+        id: 3,
         name: 'Digital diffusion',
         description: "I developed a static website for Digital diffusion.",
         tools: ['HTML','CSS', 'Bootstrap', 'javascript', 'jQuery'],
